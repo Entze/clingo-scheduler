9b2fda62a7e0274feec4bb8c71ec03f7710c1c63
Lukas Grassauer <lukas@grassauer.eu>
2024-06-05 16:29:09 +0200

    docs: allow anchors for the pandoc filter

--- docs/links.lua
+++ docs/links.lua
@@ -1,6 +1,6 @@
 
 function Link(elem)
-  if string.match(elem.target, ".+%.dj$") then
+  if string.match(elem.target, ".+%.dj") then
     local target = string.gsub(elem.target, "%.dj", ".md")
     return pandoc.Link(elem.content, target, elem.title, elem.attr)
   end
